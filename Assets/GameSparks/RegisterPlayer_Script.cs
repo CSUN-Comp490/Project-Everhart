6ac78aec0ccd524e4def3c35483ca9137684f942
ackpax <32909558+ackpax@users.noreply.github.com>
2018-04-21 23:56:06 -0700

    Merged CIT-Alex Branch with Sam's

---
+++ Assets/GameSparks/RegisterPlayer_Script.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RegisterPlayer_Script : MonoBehaviour {
+
+	public Text userNameInput;
+	public InputField passwordInput;
+
+	public Button yourButton;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(RegisterPlayerBttn);
+    }
+
+	public void RegisterPlayerBttn()
+	{
+		Debug.Log ("Registering Player...");
+		new GameSparks.Api.Requests.RegistrationRequest ()
+			.SetDisplayName (userNameInput.text)
+			.SetUserName (userNameInput.text)
+			.SetPassword (passwordInput.text)
+			.Send ((response) => {
+
+					if(!response.HasErrors)
+					{
+						Debug.Log("Player Registered \n User Name: "+response.DisplayName);
+						SceneManager.LoadScene("Menu");
+					}
+					else
+					{
+						Debug.Log("Error Registering Player... \n "+response.Errors.JSON.ToString());
+					}
+
+		});
+
+	}
+
+}
